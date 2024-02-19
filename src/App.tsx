ed078bd204521ef7b9979a5c2929f5c7808b43d6
Bach Le <tuanteam@chainslab.io>
2024-02-19 17:01:14 +0700

    chore: upgrade version, change project structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './App.css';
 
-import { Demo } from '~shared/organisms';
+import { Demo } from '~shared';
 
 const queryClient = new QueryClient();
 
